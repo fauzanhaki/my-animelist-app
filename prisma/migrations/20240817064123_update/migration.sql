d5ffe6562668eb32d43a8398d8a29666627b23eb
fauzanhaki <fauzanbaehaqi513@gmail.com>
2024-08-17 13:41:43 +0700

    update

--- prisma/migrations/20240817064123_update/migration.sql
+++ prisma/migrations/20240817064123_update/migration.sql
@@ -9,3 +9,15 @@ CREATE TABLE `Collection` (
     UNIQUE INDEX `Collection_user_email_anime_mal_id_key`(`user_email`, `anime_mal_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `anime_mal_id` VARCHAR(191) NOT NULL,
+    `user_email` VARCHAR(191) NOT NULL,
+    `comment` VARCHAR(191) NOT NULL,
+    `user_name` VARCHAR(191) NOT NULL,
+    `anime_title` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
